b642c9add1688a51520ba2a74987547fe7482252
smalaca <ab13.krakow@gmail.com>
2022-09-27 14:54:19 +0200

    add ProductRestController.add method where we use command and query

--- src/main/java/com/smalaca/productsale/api/product/ProductRestController.java
+++ src/main/java/com/smalaca/productsale/api/product/ProductRestController.java
@@ -1,6 +1,7 @@
 package com.smalaca.productsale.api.product;
 
 
+import com.smalaca.productsale.command.gateway.product.ProductCommandService;
 import com.smalaca.productsale.query.product.ProductDetailsDto;
 import com.smalaca.productsale.query.product.ProductQueryService;
 
@@ -8,12 +9,19 @@ import java.util.UUID;
 
 public class ProductRestController {
     private final ProductQueryService queryService;
+    private final ProductCommandService commandService;
 
-    public ProductRestController(ProductQueryService queryService) {
+    public ProductRestController(ProductQueryService queryService, ProductCommandService commandService) {
         this.queryService = queryService;
+        this.commandService = commandService;
     }
 
     public ProductDetailsDto displayDetails(UUID productId) {
         return queryService.displayDetails(productId);
+    }
+
+    public ProductDetailsDto add() {
+        UUID id = commandService.add();
+        return queryService.displayDetails(id);
     }
 }
---
+++ src/main/java/com/smalaca/productsale/command/gateway/product/ProductCommandService.java
@@ -0,0 +1,9 @@
+package com.smalaca.productsale.command.gateway.product;
+
+import java.util.UUID;
+
+public class ProductCommandService {
+    public UUID add() {
+        return null;
+    }
+}
